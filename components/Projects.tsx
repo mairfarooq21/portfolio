b13f7f584359789a43e9d3d02774ac04313965e1
Mair Farooq <mairfarooq21@gmail.com>
2024-09-25 00:09:10 -0700

    changes

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -67,7 +67,7 @@ const Projects = () => {
                         transform: `translateX(-${5 * index + 2}px)`,
                       }}
                     >
-                      <img src={icon} alt="icon5" className="p-2" />
+                      <Image  src={icon} alt="icon5" className="p-2" fill sizes="100vw" />
                     </div>
                   ))}
                 </div>
