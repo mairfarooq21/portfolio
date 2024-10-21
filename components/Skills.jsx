c4ef208ffbf5422cdc8570cd4d59132e18167c72
Mair Farooq <mairfarooq21@gmail.com>
2024-10-21 10:10:57 -0700

    change

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { iconLists } from "@/data";
-// import { skills } from "@/data";
 
 const Skills = () => {
     return (
@@ -8,7 +7,7 @@ const Skills = () => {
         <h1 className="heading justify-start text-3xl md:text-3xl lg:text-[43px]">
           Skills 
         </h1>
-        <div className="flex flex-wrap items-center justify-center p-4 gap-2 lg:ml-[70px] lg:mr-[74px]">
+        <div className="flex flex-wrap items-center justify-center p-4 gap-2 lg:ml-[74px] lg:mr-[74px]">
           {iconLists.map((item) => (
             <div 
               style={{paddingTop: '2px', paddingBottom: '9px'}}
@@ -19,18 +18,6 @@ const Skills = () => {
                 </div>
             </div>
           ))}
-          {/* {skills.map((item) => (
-            <div 
-              style={{paddingTop: '2px', paddingBottom: '9px'}}
-              className="px-5 lg:min-h-[10rem] h-[7.5rem] flex items-center justify-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-100 rounded-lg border border-black-300"
-              key={item.id}>
-                <div className="flex items-center justify-center mt-2">
-                  <p className="lg:text-lg md:text-xs text-sm text-white">
-                    {item.title}
-                  </p>
-                </div>
-            </div>
-          ))} */}
         </div>
       </div>
     );
